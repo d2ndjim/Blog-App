366751042b7b5ad94be0fdc1f19af90712a9f77d
Lekan <jimoridwanlekan@gmail.com>
2022-08-10 15:24:10 +0100

    Create models for database interaction

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_save :update_posts_counter
+
+  def recent_5_comments
+    comment.order(created_at: :DESC).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
+  end
+end
