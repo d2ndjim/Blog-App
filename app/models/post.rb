079014bf1418caed7c356ba4e60935bf98b7c74a
Lekan <jimoridwanlekan@gmail.com>
2022-08-11 10:20:20 +0100

    Add tests and fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,14 +5,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  
+
   after_save :update_posts_counter
 
   def recent_5_comments
     comment.order(created_at: :DESC).limit(5)
   end
 
-  private
+  # private
 
   def update_posts_counter
     author.increment!(:posts_counter)
