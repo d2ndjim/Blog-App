1bcc2c401ce3f6bb52054044794d7a0f597e8f2d
Lekan <jimoridwanlekan@gmail.com>
2022-08-11 07:55:25 +0100

    Add validations and setup rspec in gemfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-
+  
   after_save :update_posts_counter
 
   def recent_5_comments
