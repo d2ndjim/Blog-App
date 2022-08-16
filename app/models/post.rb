8063f3c5355018e200f513db723493a4412ce435
Lekan <jimoridwanlekan@gmail.com>
2022-08-16 15:19:54 +0100

    Remove comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
-  # validates :title, presence: true, length: { maximum: 250 }
-  # validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
