fc93ea33ca704b0a3cb50d1b8f610aaf99d4b324
Lekan <jimoridwanlekan@gmail.com>
2022-08-17 14:45:24 +0100

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,9 @@ class PostsController < ApplicationController
     new_post.comments_counter = 0
     respond_to do |format|
       if new_post.save
-        format.html { redirect_to "/users/#{new_post.author.id}/posts/", notice: 'Post was successfully created.' }
+        format.html do
+          redirect_to user_posts_path(user_id: new_post.author.id), notice: 'Post was successfully created.'
+        end
       else
         format.html { render :new, alert: 'Error in creating post' }
       end
