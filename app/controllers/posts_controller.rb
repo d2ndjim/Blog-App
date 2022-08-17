19ff124eb02a288c12840d5863b48a57fc79bbbe
Lekan <jimoridwanlekan@gmail.com>
2022-08-17 11:14:39 +0100

    Add forms for creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,28 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = current_user.posts.new(post_params)
+    new_post.likes_counter = 0
+    new_post.comments_counter = 0
+    new_post.update_posts_counter
+    respond_to do |format|
+      if new_post.save
+        format.html { redirect_to "/users/#{new_post.author.id}/posts/", notice: "Post was successfully created." }
+      else
+        format.html { render :new, alert: "Error in creating post" }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
