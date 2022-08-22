107b1e11ba3326070ac46945daa4dc3953d84f26
Lekan <jimoridwanlekan@gmail.com>
2022-08-22 15:15:36 +0100

    Add user show integration tests

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_153956) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users index page', type: :feature do
   before :each do
-    user1 = User.create!(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @user1 = User.create!(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     user2 = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
   end
 
@@ -16,7 +16,18 @@ RSpec.describe 'Users index page', type: :feature do
 
   it 'displays all users profile photo' do
     visit '/'
-    find('img[alt="Lilly"]')
-    find('img[alt="Tom"]')
+    expect(page).to have_content('https://unsplash.com/photos/F_-0BxGuVvo')
+  end
+
+   it 'displays number of posts' do
+    visit '/'
+    expect(page).to have_content('0')
+  end
+
+  it 'after clicking on a user, the user profile page is displayed' do
+    visit '/'
+    click_link @user1.name
+    expect(page).to have_current_path user_path(@user1)
   end
 end
+
